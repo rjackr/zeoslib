015f8b6bd0ac441be8c12b9306674bbd416865a0
marsupilami79 <marsupilami79@localhost>
2026-05-25 20:07:38 +0000

    This most probably fixes compilation on older Delphis

--- src/core/ZCompatibility.pas
+++ src/core/ZCompatibility.pas
@@ -132,6 +132,7 @@ type
   {$IF not declared(PLongBool)}
   PLongBool = ^LongBool;
   {$IFEND}
+  {$IFNDEF FPC}{$IF NOT DECLARED(size_t)}size_t = TSize_T;{$IFEND}{$ENDIF} // For older Delphis
   {$IF NOT DECLARED(Psize_t)}
   Psize_t = ^size_t;
   {$IFEND}
